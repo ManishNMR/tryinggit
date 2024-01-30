f5c418626dc6a4e30b52626027cf17435a83a0eb
Manish-0128 <2016nmr2016@gmail.com>
2024-01-30 19:03:08 +0530

    adding thisisnew

---
+++ tryinggit/thisisnew.swift
@@ -0,0 +1,18 @@
+//
+//  thisisnew.swift
+//  tryinggit
+//
+//  Created by user2 on 30/01/24.
+//
+
+import SwiftUI
+
+struct thisisnew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    thisisnew()
+}
